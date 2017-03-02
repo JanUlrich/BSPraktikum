5ed4d0cf1b019adc37c5bfabdd968eb471e5311b
Benjamin Çoban <CobbieCobbie@users.noreply.github.com>
2017-03-02 15:14:17 +0100

    Aufgabe 7 fertig
    Bash Skript und C Programm zum Vergleich der Laufzeiten

--- 7/calc_pi_compare.c
+++ 7/calc_pi_compare.c
@@ -1,5 +1,6 @@
 #define _POSIX_C_SOURCE 200112L
 
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,17 +13,27 @@ void getparams(int argc, char **argv, int *stripes);
 int main(int argc, char **argv){
 	int stripes;
 	getparams(argc,argv,&stripes);
+	printf("Berechne nun sequenziell:\n");
+	clock_t start = clock();
 	printf("Lower Bound (sequential): %.7f\n",Untersumme_seq(stripes));
 	printf("Upper Bound (sequential): %.7f\n",Obersumme_seq(stripes));
-
+	clock_t end = clock();
+	printf("Dauer: %.3f s\n",(double)(end-start)/CLOCKS_PER_SEC);
+	start = clock();
 	printf("Lower Bound (crit): %.7f\n",Untersumme_crit(stripes));
 	printf("Upper Bound (crit): %.7f\n",Obersumme_crit(stripes));
-
+	end = clock();
+	printf("Dauer: %.3f s\n",(double)(end-start)/CLOCKS_PER_SEC);
+	start = clock();
 	printf("Lower Bound (atom): %.7f\n",Untersumme_atom(stripes));
 	printf("Upper Bound (atom): %.7f\n",Obersumme_atom(stripes));
-
+	end = clock();
+	printf("Dauer: %.3f s\n",(double)(end-start)/CLOCKS_PER_SEC);
+	start = clock();
 	printf("Lower Bound (red): %.7f\n",Untersumme_red(stripes));
 	printf("Upper Bound (red): %.7f\n",Obersumme_red(stripes));
+	end = clock();
+	printf("Dauer: %.3f s\n",(double)(end-start)/CLOCKS_PER_SEC);
 }
 
 
