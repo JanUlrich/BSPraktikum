8d48c6806f7a25fa0e41430a2c4607800fbd3dce
JanUlrich <andi@michlaustderaffe.de>
2017-03-02 02:18:28 +0100

    Aufgabe 8.1

--- 8/signal.c
+++ 8/signal.c
@@ -6,38 +6,39 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <alloca.h>
+#include <stdint.h>
 
 #define MAX_LENGTH 100
+
 
 void getparams(int argc, char **argv, int *stripes);
 void registerParentSignals();
 void registerChildSignals();
-void sendByte(unsigned char b, int toPid);
+void sendByte(uint8_t b, int toPid);
 void sendSignal(int id, int sig);
 void signal_ACK(int signum);
 void signal_RECV(int signum);
-void registerStartUpSignals();
 
 int id = 0;
 int pid;
 
 //CHILD:
-volatile char status = 0;
+char status = 0;
 
 //PARENT:
 int recvCounter = 0;
 char recvByte = 0;
-char* recvData;
 
 void signal_ACK(int signum)
 {
-  if(signum==SIGUSR1){
+    registerChildSignals();
     status = 1;
-  }
 }
 
 void signal_RECV(int signum)
 {
+  registerParentSignals();
   int recvBit;
   if(signum==SIGUSR1){
     recvBit = 0;
@@ -45,14 +46,15 @@ void signal_RECV(int signum)
   if(signum==SIGUSR2){
     recvBit = 1;
   }
-  printf("%d\n", recvBit);
-  recvByte += recvBit << recvCounter%8;
+  recvByte |= recvBit << (7-recvCounter);
   recvCounter++;
   if(recvCounter%8==0){ //ganzes Byte empfangen:
     if(recvByte == 0){//Ende der Übertragung
-
+      printf("\n");
+      exit(0);
     }
-    recvData[recvCounter/8 - 1] = recvByte;
+    recvCounter=0;
+    putchar(recvByte);
     recvByte = 0;
   }
   //Bereit für Übertragung nächstes bit:
@@ -61,65 +63,60 @@ void signal_RECV(int signum)
 
 int main(int argc, char **argv)
 {
-  recvData = alloca(sizeof(char)*MAX_LENGTH);
-  registerStartUpSignals();
-
-  int fid = fork();
+  int fid = 0;
+  registerChildSignals();
+  fid = fork();
+  
   if(fid == -1){
     fprintf(stderr, "Fehler bei Fork!\n");
     return EXIT_FAILURE;
   }
 
   if(fid == 0){//CHILD
-    pid = getppid();//Diese Funktion ist immer erfolgreich
     registerChildSignals();
-    sendByte(123,pid);
+    pid = getppid();//Diese Funktion ist immer erfolgreich
+    char* sendstring="Hallo Welt";
+    while(*sendstring!='\0')
+      sendByte(*sendstring++, pid);
+    sendByte('\0',pid);
   }else{//PARENT
     id = fid;
-    while(1);
+    registerParentSignals();
+    kill(fid, SIGUSR1); //Bereit zum Empfangen
+    while(1){
+      while(status == 0){
+        sleep(1);
+      }
+    }
   }
-
-	return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
 
-void sendByte(unsigned char b, int toPid){
-  for(int i = 0; i<8;i++){
+void sendByte(uint8_t b, int toPid){
+  for(int i = 7; i>=0;i--){
     int mask = 1 << i;
-    int bit = (b & i)/i;
+    int bit = (b & mask);
     sendSignal(toPid, bit);
   }
 }
 
 void sendSignal(int id, int sig){
-  while(status == 0);
+  while(status == 0){
+    sleep(1);
+  }
+  status = 0;
   if(sig == 0){
     kill(id, SIGUSR1);
   }else{
     kill(id, SIGUSR2);
   }
-  status = 0;
 }
 
 void registerParentSignals(){
   signal(SIGUSR1, signal_RECV);
   signal(SIGUSR2, signal_RECV);
-  kill(id,SIGUSR1);
 }
 
 void registerChildSignals(){
   signal(SIGUSR1, signal_ACK);
-  kill(pid,SIGUSR1);
-}
-
-
-void signal_START(int signum)
-{
-  if(signum==SIGUSR1){
-    registerParentSignals();
-  }
-}
-
-
-void registerStartUpSignals(){
-  signal(SIGUSR1, signal_START);
 }
